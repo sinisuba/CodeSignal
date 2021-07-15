f0eea1b05e3ff017d5afed2c8baf27bf0ae5d4bd
sinisuba <68671543+sinisuba@users.noreply.github.com>
2021-07-15 03:13:23 +0200

    Deleted -> New directory rename

--- Databases/Would you LIKE the Second Meal?/suspectsInvestigation.sql
+++
@@ -1,4 +0,0 @@
-CREATE PROCEDURE suspectsInvestigation()
-BEGIN
-	SELECT id, name, surname FROM Suspect WHERE height <= 170 AND Name LIKE 'B%' AND surname LIKE 'Gre_n';
-END
--- Databases/Would you LIKE the Second Meal?/suspectsInvestigation2.sql
+++
@@ -1,8 +0,0 @@
-CREATE PROCEDURE suspectsInvestigation2()
-BEGIN
-	SELECT id, name, surname 
-    FROM Suspect 
-    WHERE height <= 170 
-    OR LOWER(name) NOT LIKE 'b%'
-    OR LOWER(surname) NOT LIKE 'gre_n';
-END
